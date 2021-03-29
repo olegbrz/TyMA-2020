bda03fec08d79878946fa16c60dd34f597268bb2
Oleg <brezitskyyoleg@gmail.com>
2021-03-29 02:53:05 +0200

    minor change

--- src/hist.py
+++ src/hist.py
@@ -1,5 +1,6 @@
 from typing import List
 import matplotlib.pyplot as plt
+from operator import add
 import numpy as np
 import sys
 
@@ -42,17 +43,30 @@ def get_skew_diag(dotplot):
     return counts
 
 
-fig, axs = plt.subplots(2, figsize=(10, 6))
-fig.suptitle(f"HITS HISTOGRAM: {sys.argv[1]}")
-axs[0].set_title("Hits for main diagonals [↘]")
-axs[0].plot(get_diag(dotplot), linewidth=0.5, color="red")
-axs[1].set_title("Hits for skew diagonals [↙]")
-axs[1].plot(get_skew_diag(dotplot), linewidth=0.5, color="blue")
+pdiags = get_diag(dotplot)
+rdiags = get_skew_diag(dotplot)
 
-max_y = max(axs[0].get_ylim()[1], axs[1].get_ylim()[1])
+plt.style.use("classic")
+
+fig, axs = plt.subplots(3, figsize=(6, 9))
+title = sys.argv[1].split("\\")[-1]
+fig.suptitle(f"HITS HISTOGRAM: {title}", fontweight="bold")
+axs[0].set_title("Hits for main diagonals [↘]")
+axs[0].plot(pdiags, linewidth=1, color="r")
+axs[1].set_title("Hits for skew diagonals [↙]")
+axs[1].plot(rdiags, linewidth=1, color="g")
+axs[2].set_title("Sum")
+axs[2].plot(
+    list(map(add, pdiags, rdiags)),
+    linewidth=1,
+    color="k",
+)
+
+max_y = max(axs[0].get_ylim()[1], axs[1].get_ylim()[1], axs[2].get_ylim()[1])
 
 for ax in axs:
-    ax.set(xlabel="Diagonal number", ylabel="Number of hits", ylim=[0, max_y])
+    ax.set(xlabel="Diagonal number", ylabel="Number of hits", ylim=[-10, max_y])
     ax.label_outer()
+    ax.grid(True)
 
 plt.savefig(sys.argv[1] + "_hist.png", dpi=300, bbox_inches="tight")
